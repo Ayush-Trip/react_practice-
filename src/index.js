52522893ed57a7144e8ab3189c7fc510b81b22da
Ayush Tripathi <ayushtripathi1012@gmail.com>
2023-09-25 01:36:02 +0530

    Learning CSS in React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDOM from 'react-dom';
+ import './index.css'; 
+const name = "Ayush";
+const img1="https://picsum.photos/200/300"
+const img2="https://picsum.photos/250/300"
+const img3="https://picsum.photos/300/300"
+const linkedin= "https://www.linkedin.com/in/ayush-tripathi-762a57239/"
+ReactDOM.render(
+    <>
+    <h1 className= "heading"  >My name is {name} </h1>
+    <div className='img_div'>
+    <img src={img1} alt = "Random Images"/>
+    <img src={img2} alt = "Random Images"/>
+    <a href = {linkedin} target = "_blank">
+    <img src={img3} alt = "Random Images"/>
+    </a>
+    </div>
+    </>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
